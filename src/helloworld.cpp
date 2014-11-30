d908a43b28b166c8a7e0613f6c63fc19e0a2c18b
Kevin Michael Allen <kma1660@gmail.com>
2014-11-30 00:53:10 -0500

    new file:   .cproject 	new file:   .project 	new file:   Debug/helloworld 	new file:   Debug/makefile 	new file:   Debug/objects.mk 	new file:   Debug/sources.mk 	new file:   Debug/src/helloworld.d 	new file:   Debug/src/helloworld.o 	new file:   Debug/src/subdir.mk 	new file:   src/helloworld.cpp

---
+++ src/helloworld.cpp
@@ -0,0 +1,17 @@
+//============================================================================
+// Name        : helloworld.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	string x = "test";
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	cout << x << endl;
+	return 0;
+}
