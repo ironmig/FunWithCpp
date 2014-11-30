6df88190c075ba4a9c988e1e9ea9a8da1253a340
Kevin Michael Allen <kma1660@gmail.com>
2014-11-30 16:53:34 -0500

    modified:   Debug/helloworld 	modified:   Debug/src/helloworld.o 	new file:   install_ubuntu.sh 	modified:   src/helloworld.cpp

--- src/helloworld.cpp
+++ src/helloworld.cpp
@@ -8,10 +8,45 @@
 
 #include <iostream>
 using namespace std;
+class Animal {
+	protected:
+		float weight;
+	public:
+		float getWeight () {
+			return weight;
+		}
+		Animal () {
+			weight = 10;
+		}
+		Animal (int x) {
+			weight = x;
+		}
+};
+class Person : public Animal {
+private:
+	int age;
+public:
+	int getAge () {
+		return age;
+	}
+	Person () {
+		age = 22;
+		weight=100;
+	}
+	Person (float newweight, int newage) {
+		age = newage;
+		weight = newweight;
+	}
+};
 
 int main() {
-	string x = "test";
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
-	cout << x << endl;
+	int age;
+	float weight;
+	cout << "What is your age?" << endl;
+	cin >> age;
+	cout << "How much do you weigh?" <<endl;
+	cin >> weight;
+	Person bob(weight,age);
+	cout<<bob.getAge()<<endl;
 	return 0;
 }
